af6c8525d629ffff43fa9d490a7da34f0926e1dc
evren Önen <evr.onen@gmail.com>
2022-08-23 11:14:41 +0300

    eee

---
+++ app/Http/Controllers/PersonController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Person;
+use Illuminate\Http\Request;
+
+class PersonController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $validator = validator()->make(request()->all(), [
+            'name'      => 'required | string',
+            'book_id'     => 'required | email',
+        ]);
+
+        if ($validator->fails()) {
+            return response($validator->errors());
+        }
+        $req->all();
+        $writer = new Writer();
+        $writer->name= $req->name;
+        $writer->book_id= $req->book_id;
+        $writer->save();
+        return $writer;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Person  $person
+     * @return \Illuminate\Http\Response
+     */
+    public function get(Person $person)
+    {
+        return Person::all();
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Person  $person
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Person $person)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Person  $person
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Person $person)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Person  $person
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Person $person)
+    {
+        //
+    }
+}
